0df9c326518c33b6da598156787c5769c48581f5
Nnamdi Michael Okpala <support@obinexus.org>
2025-07-02 04:28:16 +0100

    W

--- rift-0/include/rift-0/core/lexer/lexer_flag.h
+++ rift-0/include/rift-0/core/lexer/lexer_flag.h
@@ -1,6 +1,17 @@
 #ifndef LEXER_FLAG_H
 #define LEXER_FLAG_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+// =====================
+// Lexer Flag API (modular, thread-safe)
+// =====================
+#include "rift-0/core/lexer/lexer.h"
+
+void lexer_set_flag(LexerContext* ctx, Lexer
 #include <stdbool.h>
 #include <stddef.h>
 #include <stdint.h>
@@ -26,16 +37,15 @@ void hq_destroy(HeapQueue* hq);
 void hq_enqueue(HeapQueue* hq, int priority, int value);
 Item hq_dequeue(HeapQueue* hq);
 
-// =====================
-// Lexer Flag API (modular, thread-safe)
-// =====================
-#include "rift-0/core/lexer/lexer.h"
-
-void lexer_set_flag(LexerContext* ctx, LexerFlags flag);
+Flags flag);
 void lexer_clear_flag(LexerContext* ctx, LexerFlags flag);
 bool lexer_flag_enabled(const LexerContext* ctx, LexerFlags flag);
 
 TokenNode* token_create(TokenType type, const char* value, size_t length);
 void token_destroy(TokenNode* token);
+
+#ifdef __cplusplus
+}
+#endif // End of extern "C"
 
 #endif // LEXER_FLAG_H
