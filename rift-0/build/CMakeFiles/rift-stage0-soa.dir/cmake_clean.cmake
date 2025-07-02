52b2748e94777b46caabe5b2eb3e1ed51541b68d
Nnamdi Michael Okpala <support@obinexus.org>
2025-07-02 03:17:51 +0100

    rift-0

--- rift-0/build/CMakeFiles/rift-stage0-soa.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/rift-stage0-soa"
-  "lib/rift-stage0.so.a"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/rift-stage0-soa.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- rift-0/build/CMakeFiles/rift-stage0-static.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "obj/librift-stage0.a"
-)
